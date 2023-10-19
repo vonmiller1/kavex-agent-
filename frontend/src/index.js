511476e6204cf7a010c1cf86478860b262109a41
Chukwuma Mainno <chukwumanwafor485@gmail.com>
2023-10-19 08:20:00 -0700

    Add React entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-const container = document.getElementById('root');
+const container = document.getElementById('rooot');
 const root = createRoot(container);
 root.render(<App />);
