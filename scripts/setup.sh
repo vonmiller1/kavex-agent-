9912693636e79d260479a137750e294754af2e91
Chukwuma Mainno <chukwumanwafor485@gmail.com>
2023-10-26 16:00:00 -0700

    Add setup helper script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Run backend:'
-echo '  python -m venv .venv && source .venv/bin/activate'
+echo '  python -m venv .venv && source .venv/bin/actvate'
 echo '  pip install -r backend/requirements.txt'
 echo '  uvicorn backend.app.main:app --reload --port 8000'
